78f93267bde2d1418233a8cd352cab50caec4111
Zohrab <116635367+zohrab446@users.noreply.github.com>
2025-01-25 14:04:19 +0300

    Last version
    Added response time and time animation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ui = new UI();
 
 ui.btn_start.addEventListener("click", function() {
     ui.quiz_box.classList.add("active");
+    startTimer(10);
+    startTimerLine();
     ui.soruGoster(quiz.soruGetir());
     ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
     ui.btn_next.classList.remove("show");
@@ -11,11 +13,16 @@ ui.btn_start.addEventListener("click", function() {
 ui.btn_next.addEventListener("click", function() {
     if (quiz.sorular.length != quiz.soruIndex + 1) {
         quiz.soruIndex += 1;
+        clearInterval(counter);
+        clearInterval(counterLine);
+        startTimer(10);
+        startTimerLine();
         ui.soruGoster(quiz.soruGetir());
         ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
         ui.btn_next.classList.remove("show");
     } else {
-        console.log("quiz bitti");
+        clearInterval(counter);
+        clearInterval(counterLine);
         ui.quiz_box.classList.remove("active");
         ui.score_box.classList.add("active");
         ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
@@ -35,6 +42,8 @@ ui.btn_replay.addEventListener("click", function() {
 
 
 function optionSelected(option) {
+    clearInterval(counter);
+    clearInterval(counterLine);
     let cevap = option.querySelector("span b").textContent;
     let soru = quiz.soruGetir();
 
@@ -52,6 +61,52 @@ function optionSelected(option) {
     }
 
     ui.btn_next.classList.add("show");
+}
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.time_second.textContent = time;
+        time--;
+
+        if(time < 0) {
+            clearInterval(counter);
+
+            ui.time_text.textContent = "Süre Bitti";
+
+            let cevap = quiz.soruGetir().dogruCevap;
+
+            for(let option of ui.option_list.children) {
+
+                if(option.querySelector("span b").textContent == cevap) {
+                    option.classList.add("correct");
+                    option.insertAdjacentHTML("beforeend", ui.correctIcon);
+                }
+
+                option.classList.add("disabled");
+            }
+
+            ui.btn_next.classList.add("show");
+        }
+    }
+}
+
+let counterLine;
+function startTimerLine() {
+    let line_width = 0;
+
+    counterLine = setInterval(timer, 20);
+
+    function timer() {
+        line_width += 1;
+        ui.time_line.style.width = line_width + "px";
+
+        if(line_width > 549) {
+            clearInterval(counterLine);
+        }
+    }
 }
 
 
